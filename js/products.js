e9ed36b809fdca481e62d3cea78b3e812478c1d8
cavi26 <88489804+cavi26@users.noreply.github.com>
2021-08-19 19:00:44 -0300

    Add files via upload

--- js/products.js
+++ js/products.js
@@ -2,5 +2,28 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(produ => {
+        infoProductos = produ.data;
+        let htmlContentToAppend = '';
+        for (info of infoProductos) {
+            htmlContentToAppend += `
+            <a href="products.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + info.imgSrc + `" alt="` + info.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ info.name +`</h4>
+                            <small class="text-muted">` + info.soldCount+ ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + info.description + `</p>
+                        <p class="mb-1"> Costo: $ ` + info.cost + '' + info.currency + `</p>
+                    </div>
 
+                </div>
+            </a>  `
+        }
+        document.getElementById ('lista_de_productos').innerHTML = htmlContentToAppend
+    });
 });
